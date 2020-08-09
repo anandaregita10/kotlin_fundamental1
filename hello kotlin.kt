0dcc0e8fda5348317eaa0533352c99ca8bcdeb2d
anandaregita <anandaregita05@gmail.com>
2020-08-09 12:14:17 +0700

    initial commit

---
+++ hello kotlin.kt
@@ -0,0 +1,12 @@
+fun main() {
+     val name = "Ananda Regita Setiawan"
+     val hobby = "Memancing "
+
+
+         print( "Nama saya ")
+         println(name)
+         print( "Hobi saya ")
+         println(hobby)
+
+}
+
